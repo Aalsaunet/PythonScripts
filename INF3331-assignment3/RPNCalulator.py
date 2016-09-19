c100f39a8f852608f50939d3755f63d58666a837
Andreas Oven Aalsaunet <andreas.aalsaunet@gmail.com>
2016-09-19 18:14:15 +0200

    Added a readme for assignment3 and the start of the RPNCalculator

---
+++ INF3331-assignment3/RPNCalulator.py
@@ -0,0 +1,4 @@
+# This calculator script makes use of Reverse Polish Notation to calculate values.
+# Inputted numbers are pushed on a stack and a operators pops the appropriate numbers/operands from the stack and pushes
+# the result on the stack. "P" prints the last number on the stack without popping it.
+
